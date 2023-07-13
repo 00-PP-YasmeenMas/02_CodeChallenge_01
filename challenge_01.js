cf7d071ea2eccb924104be56e0224fc7eecf5a3b
YasmeenMas <yasmeenmas@gmx.de>
2023-07-13 19:26:59 +0200

    eine Lösung

--- challenge_01.js
+++ challenge_01.js
@@ -1,6 +1,5 @@
-
 /*********   CODE CHALLENGE 01 **********/
- 
+
 /*
 Geben Sie über eine Prompt-Anforderung eine Zahl ein.
 Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
@@ -9,9 +8,9 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
-let zahl1 = prompt ("Bitte erste Zahl eingeben!");
-let zahl2 = prompt ("Bitte zweite Zahl eingeben!");
+let zahl1 = prompt("Bitte erste Zahl eingeben!");
+let zahl2 = prompt("Bitte zweite Zahl eingeben!");
 
-let summe = zahl1 + zahl2 ;
+let summe = parseInt(zahl1) + parseInt(zahl2);
 
 console.log("Die Summe der Zahlen ist: " + summe);
